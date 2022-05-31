254c257e6c56bdb77905685305532f9be0eb1181
Emmanuel Hateka <emmanuelhateka.com>
2022-05-31 14:07:00 +0000

    created hotel model

--- api/index.js
+++ api/index.js
@@ -2,9 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.js";
-// import usersRoute from "./routes/users.js";
-// import hotelsRoute from "./routes/hotels.js";
-// import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 dotenv.config();
 const app = express();
@@ -20,9 +20,9 @@ const connect = async () => {
 
 // middlewares
 app.use("/api/auth", authRoute);
-// app.use("/api/users", usersRoute);
-// app.use("/api/hotels", hotelsRoute);
-// app.use("/api/rooms", roomsRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.listen(process.env.PORT, () => {
   connect();
